85675c9857db9414ecf542890f5758253b9aa02e
ganareynara48 <ganareynara@gmail.com>
2024-02-18 15:23:44 +0700

    x

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const routes  = require('./config/router');
 const bodyParser = require('body-parser');
 
 const app = express();
 const port = 3000;
+
+app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
